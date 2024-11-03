962912954999a23d4b35a9986d7d97018471a09e
Benjamin Stacey <117607406+SwiftCham@users.noreply.github.com>
2024-11-03 01:01:35 +0000

    Created Actic based http service
    included the Actix framework and created a basic endpoint skeleton for the http service.

--- forseti-service/src/main.rs
+++ forseti-service/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+//Third-party-dependencies
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // address the server will run on
+    let address = "127.0.0.1:9090";
+    println!("Server started at {}", address);
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind(address)?
+        .run()
+        .await
+}
+//TEST: Launch server on http://127.0.0.1:9090/
+
+//CRUD OPERATIONS
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, Laminotes!")
 }
